272e3afde4d467d80bb921ac9de0bf2042244af9
Grégoire Paulet <gregoirepaulet84@gmail.com>
2023-09-19 11:59:11 +0200

    register/login interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from '@styles/theme';
 
+import Register from './views/Auth/Register';
+import Login from './views/Auth/Login';
 import Film from './views/Film';
 import Home from './views/Home';
 import About from './views/About';
@@ -13,6 +15,8 @@ export function App() {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
             <Route path="/film/:id" element={<Film />} />
             <Route path="/about" element={<About />} />
